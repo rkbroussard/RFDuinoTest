f0db146e772ac4234cbbaaede59eed1455b1e8f6
Kyle Broussard <rkbroussard@gmail.com>
2014-11-21 16:35:55 -0600

    Re-arrange files for eclipse and test with genymotion

--- src/main/src/com/lannbox/rfduinotest/HexAsciiHelper.java
+++ src/main/src/com/lannbox/rfduinotest/HexAsciiHelper.java
@@ -1,15 +1,6 @@
 package com.lannbox.rfduinotest;
 
-import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
-
 import org.apache.http.util.ByteArrayBuffer;
-
-import java.util.regex.Pattern;
 
 public class HexAsciiHelper {
     public static int PRINTABLE_ASCII_MIN = 0x20; // ' '
